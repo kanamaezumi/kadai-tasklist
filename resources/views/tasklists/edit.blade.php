67d03a4be90dd3789e00b09c771b91fce7d92816
kana.maezumi <kana.a.maezumi@rakuten.com>
2018-06-01 06:12:44 +0000

    bootstrap design

--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -5,16 +5,21 @@
 <!-- Write content for each page here -->
 
     <h1>id: {{ $tasklist->id }} のタスクの編集ページ</h1>
-
+<div class="row">
+    <div class="col-xs-6">
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
- {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
+       <div class="form-group">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
+    </div>
+    </div>
 
 @endsection
