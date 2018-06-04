ae58c1c5d32a33752796bf453d2a43e7230d95f3
kana.maezumi <kana.a.maezumi@rakuten.com>
2018-06-04 00:47:12 +0000

    modfied grid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
           @include('commons.navbar')
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-offst-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+                    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
                          @include('commons.error_tasklists')
                          @yield('content')
                     </div>
